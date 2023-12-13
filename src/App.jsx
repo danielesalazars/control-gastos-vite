62b8cc5d9f6dd6eb6a74ba0eec1c9b4925cddb68
Daniel Salazar <daniel.esalazars@gmail.com>
2023-12-13 16:39:10 -0500

    Primer commit del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import ListadoGastos from "./components/ListadoGastos";
+import Modal from "./components/Modal";
+import { generarId } from "./helpers";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
+
+function App() {
+  const [gastos, setGastos] = useState([]);
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  const [gastoEditar, setGastoEditar] = useState({});
+
+  useEffect(() => {
+    console.log(gastos);
+    if (Object.keys(gastoEditar).length > 0) {
+      setModal(true);
+    }
+  }, [gastoEditar]);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+    setGastoEditar({});
+  };
+
+  const guardarGasto = (gasto) => {
+    if (gasto.id) {
+      const gastosActualizados = gastos.map((gastoState) =>
+        gastoState.id === gasto.id ? gasto : gastoState
+      );
+      setGastos(gastosActualizados);
+      setGastoEditar({});
+    } else {
+      gasto.id = generarId();
+      gasto.fecha = Date.now();
+      setGastos([...gastos, gasto]);
+    }
+  };
+
+  const eliminarGasto = (id) => {
+    const gastosActualizados = gastos.filter((gasto) => gasto.id !== id);
+    setGastos(gastosActualizados);
+  };
+
+  return (
+    <div className={modal ? "fijar" : ""}>
+      <Header
+        gastos={gastos}
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+
+      {isValidPresupuesto && (
+        <>
+          <main>
+            <ListadoGastos
+              gastos={gastos}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+            />
+          </main>
+          <div className="nuevo-gasto">
+            <img
+              src={IconoNuevoGasto}
+              alt="Icono nuevo gasto"
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
+      )}
+
+      {modal && (
+        <Modal
+          setModal={setModal}
+          guardarGasto={guardarGasto}
+          gastoEditar={gastoEditar}
+          setGastoEditar={setGastoEditar}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
